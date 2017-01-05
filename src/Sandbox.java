6b2ff84a0dff9eeffd4f2ac562e16bf19a16bbb1
Andre <andrehsu6@gmail.com>
2017-01-06 01:19:21 +0800

    Test for ClassCastException for Object.equals(Object o)

---
+++ src/Sandbox.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+/**
+ * Created by Andre on 1/6/2017.
+ */
+public class Sandbox {
+	public static void main(String[] args){
+		System.out.println(BigInteger.ZERO.equals("asdf"));
+	}
+}
