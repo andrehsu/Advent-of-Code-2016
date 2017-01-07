e3ace7eb1d1e7ac4b1080e3f08b755fe539e5e91
Andre <andrehsu6@gmail.com>
2017-01-07 08:32:01 +0800

    day 20 part 2

--- Day 20/src/ValidIP.java
+++ Day 20/src/ValidIP.java
@@ -17,7 +17,7 @@ public class ValidIP {
 			for (Iterator<String> iterator = bannedRanges.iterator(); iterator.hasNext(); ) {
 				String next = iterator.next();
 				
-				long[] ranges = ranges(next);
+				long[] ranges = range(next);
 				if (ranges[0] <= smallestValue && smallestValue <= ranges[1]) {
 					smallestValue = ranges[1] + 1;
 					iterator.remove();
@@ -28,7 +28,42 @@ public class ValidIP {
 		return smallestValue;
 	}
 	
-	private static long[] ranges(String input) {
+	public static long numberOfValidIPs(List<String> bannedRanges) {
+		mergeRanges(bannedRanges);
+		
+		long sumRemove = 0;
+		for (String s : bannedRanges) {
+			long[] range = range(s);
+			sumRemove += range[1] - range[0] + 1;
+		}
+		
+		return 4294967295L + 1 - sumRemove;
+	}
+	
+	private static void mergeRanges(List<String> IPs) {
+		boolean done;
+		outer:
+		do {
+			done = true;
+			for (String s1 : new LinkedList<>(IPs)) {
+				long[] range1 = range(s1);
+				for (String s2 : new LinkedList<>(IPs)) {
+					if (s1.equals(s2)) continue;
+					
+					long[] range2 = range(s2);
+					if (range1[0] <= range2[0] && range2[0] <= range1[1]) {
+						IPs.remove(s1);
+						IPs.remove(s2);
+						IPs.add(range1[0] + "-" + (range2[1] > range1[1] ? range2[1] : range1[1]));
+						done = false;
+						continue outer;
+					}
+				}
+			}
+		} while (!done);
+	}
+	
+	private static long[] range(String input) {
 		String[] token_strings = input.trim().split("-");
 		return new long[]{Long.parseLong(token_strings[0]),
 				Long.parseLong(token_strings[1])};
@@ -46,5 +81,11 @@ public class ValidIP {
 class RunDay20_Part1 {
 	public static void main(String[] args) {
 		System.out.println(ValidIP.lowestValidIP(ValidIP.input));
+	}
+}
+
+class RunDay20_Part2 {
+	public static void main(String[] args) {
+		System.out.println(ValidIP.numberOfValidIPs(ValidIP.input));
 	}
 }
