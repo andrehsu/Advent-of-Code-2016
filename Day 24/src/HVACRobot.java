dd4147b304fa05b58ea21fe42306712dde1d90ed
Andre <andrehsu6@gmail.com>
2017-01-11 14:40:08 +0800

    added day 24

---
+++ Day 24/src/HVACRobot.java
@@ -0,0 +1,21 @@
+import andre.adventofcode.input.Input;
+import com.google.common.collect.Table;
+
+import java.util.List;
+
+/**
+ * Created by Andre on 1/11/2017.
+ */
+public class HVACRobot {
+	public static final List<String> input = Input.readAllLines("Day 24/input.txt");
+	
+	private static Table<Integer, Integer, Integer> generateAdjacencyTable(List<String> input) {
+		
+	}
+	
+	private static class Test {
+		public static void main(String[] args) {
+			
+		}
+	}
+}
