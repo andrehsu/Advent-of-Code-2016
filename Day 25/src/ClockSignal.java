342007f8b1e30345bde82a4cc45238b84abdac04
Andre <andrehsu6@gmail.com>
2017-01-24 21:43:30 +0800

    standardized interpreter

--- Day 25/src/ClockSignal.java
+++ Day 25/src/ClockSignal.java
@@ -1,7 +1,9 @@
 import andre.adventofcode.input.Input;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Andre on 1/24/2017.
@@ -11,20 +13,21 @@ public class ClockSignal {
 	
 	private final List<String> code;
 	private final int initializer;
+	private final int firstNOutput;
+	private final Map<Character, Integer> registers = new HashMap<>();
+	private final StringBuilder output = new StringBuilder();
 	
-	private int a,
-			b = 0,
-			c = 0,
-			d = 0;
-	
-	private ClockSignal(List<String> code, int a) {
+	private ClockSignal(List<String> code, int a, int firstNOutput) {
 		this.code = code;
-		this.a = a;
+		registers.put('a', a);
+		registers.put('b', 0);
+		registers.put('c', 0);
+		registers.put('d', 0);
 		this.initializer = a;
+		this.firstNOutput = firstNOutput;
 	}
 	
-	private String firstNOutput(int n) {
-		final StringBuilder output = new StringBuilder();
+	private String firstNOutput() {
 		List<String[]> pregenTokens = new ArrayList<>();
 		for (String s : code) {
 			pregenTokens.add(s.trim().split(" "));
@@ -34,22 +37,20 @@ public class ClockSignal {
 			String[] tokens = pregenTokens.get(line);
 			switch (tokens[0]) {
 				case "cpy":
-					copy(tokens[2], tokens[1]);
+					copy(tokens);
 					break;
 				case "inc":
-					increment(tokens[1]);
+					increment(tokens);
 					break;
 				case "dec":
-					decrement(tokens[1]);
+					decrement(tokens);
 					break;
 				case "jnz":
-					if (parseParam(tokens[1]) != 0) {
-						line = line - 1 + parseParam(tokens[2]);
-					}
+					line = line + jnz(tokens) - 1;
 					break;
 				case "out":
-					output.append(parseParam(tokens[1]));
-					if (output.length() == n)
+					output(tokens);
+					if (output.length() == firstNOutput)
 						return output.toString();
 					break;
 			}
@@ -58,71 +59,49 @@ public class ClockSignal {
 		return output.toString();
 	}
 	
-	private void increment(String register) {
-		switch (register.charAt(0)) {
-			case 'a':
-				a++;
-				break;
-			case 'b':
-				b++;
-				break;
-			case 'c':
-				c++;
-				break;
-			case 'd':
-				d++;
-				break;
-		}
+	private void increment(String[] tokens) {
+		char c = tokens[1].charAt(0);
+		if (registers.containsKey(c))
+			registers.merge(c, 1, Integer::sum);
+		else
+			throw new RuntimeException("Invalid param for inc");
 	}
 	
-	private void decrement(String register) {
-		switch (register.charAt(0)) {
-			case 'a':
-				a--;
-				break;
-			case 'b':
-				b--;
-				break;
-			case 'c':
-				c--;
-				break;
-			case 'd':
-				d--;
-				break;
-		}
+	private void decrement(String[] tokens) {
+		char c = tokens[1].charAt(0);
+		if (registers.containsKey(c))
+			registers.merge(c, -1, Integer::sum);
+		else
+			throw new RuntimeException("Invalid param for dec");
 	}
 	
-	private void copy(String register, String param) {
-		switch (register.charAt(0)) {
-			case 'a':
-				a = parseParam(param);
-				break;
-			case 'b':
-				b = parseParam(param);
-				break;
-			case 'c':
-				c = parseParam(param);
-				break;
-			case 'd':
-				d = parseParam(param);
-				break;
-		}
+	private void copy(String[] tokens) {
+		String from = tokens[1];
+		char to = tokens[2].charAt(0);
+		if (registers.containsKey(to)) {
+			registers.put(to, parseParam(from));
+		} else
+			throw new RuntimeException("Invalid param for cpy");
+	}
+	
+	private void output(String[] tokens) {
+		output.append(parseParam(tokens[1]));
+	}
+	
+	private int jnz(String[] tokens) {
+		if (parseParam(tokens[1]) != 0) {
+			return parseParam(tokens[2]);
+		} else
+			return 1;
 	}
 	
 	private int parseParam(String param) {
 		try {
 			return Integer.parseInt(param);
 		} catch (NumberFormatException e) {
-			switch (param.charAt(0)) {
-				case 'a':
-					return a;
-				case 'b':
-					return b;
-				case 'c':
-					return c;
-				case 'd':
-					return d;
-			}
+			char c = param.charAt(0);
+			if (registers.containsKey(c))
+				return registers.get(c);
 		}
 		
 		throw new RuntimeException("Invalid param");
@@ -130,8 +109,8 @@ public class ClockSignal {
 	
 	public static int lowestInitializer(List<String> input) {
 		for (int i = 1; i < 10_000; i++) {
-			ClockSignal instance = new ClockSignal(input, i);
-			if (instance.firstNOutput(12).equals("010101010101"))
+			ClockSignal instance = new ClockSignal(input, i, 12);
+			if (instance.firstNOutput().equals("010101010101"))
 				return i;
 		}
 		
@@ -140,8 +119,8 @@ public class ClockSignal {
 	
 	private static class Test {
 		public static void main(String[] args) {
-			ClockSignal instance = new ClockSignal(input, 198);
-			System.out.println(instance.firstNOutput(25));
+			ClockSignal instance = new ClockSignal(input, 198, 25);
+			System.out.println(instance.firstNOutput());
 		}
 	}
 }
