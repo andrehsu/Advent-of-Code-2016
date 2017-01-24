d6ae22279231aabf93d41f431b66e5fa0dd7a452
Andre <andrehsu6@gmail.com>
2017-01-24 15:25:25 +0800

    day 24 first commit

---
+++ Day 24/src/AirDuctMaze.java
@@ -0,0 +1,16 @@
+import andre.adventofcode.input.Input;
+
+import java.util.List;
+
+/**
+ * Created by Andre on 1/24/2017.
+ */
+public class AirDuctMaze {
+	public static final List<String> input = Input.readAllLines("Day 24/input.txt");
+}
+
+class RunDay24Part1 {
+	public static void main(String... args) {
+		
+	}
+}
