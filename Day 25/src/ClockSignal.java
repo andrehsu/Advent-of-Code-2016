bb7a31a62510f101c216b0ebed302a1445fe1d7f
Andre <andrehsu6@gmail.com>
2017-01-24 17:16:43 +0800

    day 25 first commit

---
+++ Day 25/src/ClockSignal.java
@@ -0,0 +1,17 @@
+import andre.adventofcode.input.Input;
+
+import java.util.List;
+
+/**
+ * Created by Andre on 1/24/2017.
+ */
+public class ClockSignal {
+	public static final List<String> input = Input.readAllLines("Day 25/input.txt");
+	
+}
+
+class RunDay25Part1 {
+	public static void main(String... args) {
+		
+	}
+}
